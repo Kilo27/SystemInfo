176923cc52315662c3f9f3af26c129cf8b614b32
Kyle Joyce <kyle.oseoighe@gmail.com>
2025-10-27 19:47:47 +0000

    Manually converting functions still. CPU and Memory complete so far. Ignore the warnings will fix themselves when I integrate into app.java

--- app/src/main/java/org/example/OshiMethod.java
+++ app/src/main/java/org/example/OshiMethod.java
@@ -163,23 +163,100 @@ class OshiMethod {
     }
 
 }
-class SysCPU extends ProcessorSpecs{
-    long[] freqs = cpu.getCurrentFreq();
-    long maxfreq = cpu.getMaxFreq();
-    double[] cpuLoad = cpu.getProcessorCpuLoad(1000);
-    List<PhysicalProcessor> phyproc = cpu.getPhysicalProcessors();
-    List<LogicalProcessor> logproc = cpu.getLogicalProcessors();
+class CPUGeneric extends ProcessorSpecs{
+    CPUGeneric(){
+        super();
+    }
+    protected long[] freqs = cpu.getCurrentFreq();
+    protected long maxfreq = cpu.getMaxFreq();
+    protected double[] cpuLoad = cpu.getProcessorCpuLoad(1000);
+    protected List<PhysicalProcessor> physproc = cpu.getPhysicalProcessors();
+    protected List<LogicalProcessor> logproc = cpu.getLogicalProcessors();
+    public String getCurrentFrequency(int i){
+        return FormatUtil.formatHertz(freqs[i]);
+    }
+    public String getMaxFrequency(int i){
+        return FormatUtil.formatHertz(maxfreq);
+    }
+    //I need to add ticks here but will do tomorrow because this is killing me slowly
+}
+class PhysicalCPU extends CPUGeneric {
+    PhysicalCPU(){
+        super();
+    }
     public int physicalProcessorCount(){
         return cpu.getPhysicalProcessorCount();
     }
     public String getIdString(int i){
-        return phyproc.get(i).getIdString();
+        return physproc.get(i).getIdString();
     }
     public int getPackageNumber(int i){
-        return phyproc.get(i).getPhysicalPackageNumber();
+        return physproc.get(i).getPhysicalPackageNumber();
+    }
+    public int processorEfficiency(int i){
+        return physproc.get(i).getEfficiency();
+    }
+    public void testData(){
+        System.out.printf("%-25s | %s\n","Processor ID: " , getIdString(0));
+        System.out.printf("%-25s | %s\n","Processor Package Number: " , getPackageNumber(0));
+        System.out.printf("%-25s | %s\n","Processor Core ID:" , physicalProcessorCount());
+        System.out.printf("%-25s | %s\n", "Processor Efficiency: " , processorEfficiency(0));
+        /*
+        System.out.println(physicalProcessorCount());
+        System.out.println(getIdString(0));
+        System.out.println(getPackageNumber(0));
+        System.out.println(processorEfficiency(0));
+        */
+        System.out.println(getCurrentFrequency(0));
+        System.out.println(getMaxFrequency(0));
+    }
+}
+class LogicalCPU extends CPUGeneric {
+    LogicalCPU(){
+        super();
+    }
+    public int logicalProcessorCount(){
+        return cpu.getLogicalProcessorCount();
+    }
+    public int NumaNode(int i){
+        return logproc.get(i).getNumaNode();
+    }
+    //Not sure about these two functions below. Are you sure these refer to logical processors and not physical ones?
+    public int packageNumber(int i){
+        return logproc.get(i).getPhysicalPackageNumber();
+    }
+    public int physicalProcessorNumber(int i){
+        return logproc.get(i).getPhysicalProcessorNumber();
+    }
+    public int processorGroup(int i){
+        return logproc.get(i).getProcessorGroup();
+    }
+    public int processorNumber(int i){
+        return logproc.get(i).getProcessorNumber();
+
+    }
+    public void testData(){
+        System.out.printf("%-25s | %s\n", "NUMA Node: " , NumaNode(0));
+        System.out.printf("%-25s | %s\n", "Package Number: " , packageNumber(0));
+        System.out.printf("%-25s | %s\n","Processor Number: " , physicalProcessorNumber(0));
+        System.out.printf("%-25s | %s\n","Processor Group", processorGroup(0));
+        System.out.printf("%-25s | %s\n","Processor Number", processorNumber(0));
+        /*
+        System.out.println(logicalProcessorCount());
+        System.out.println(NumaNode(0));
+        System.out.println(packageNumber(0));
+        System.out.println(physicalProcessorNumber(0));
+        System.out.println(processorGroup(0));
+        System.out.println(processorNumber(0));
+        */
+        System.out.println(getCurrentFrequency(0));
+        System.out.println(getMaxFrequency(0));
     }
 }
 class ProcessorSpecs extends OshiMethod{
+    ProcessorSpecs(){
+        super();
+    }
     public String name(){
         return cpu.getProcessorIdentifier().getName();
     }
