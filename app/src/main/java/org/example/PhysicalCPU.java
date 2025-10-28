67e2d841b6e706e52344cd0a0f3f54a00a3dd3d8
Kyle Joyce <kyle.oseoighe@gmail.com>
2025-10-28 15:22:52 +0000

    Breaking everything into separate packages

---
+++ app/src/main/java/org/example/PhysicalCPU.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class PhysicalCPU extends CPUGeneric {
+    PhysicalCPU(){
+        super();
+    }
+    public int physicalProcessorCount(){
+        return cpu.getPhysicalProcessorCount();
+    }
+    public String getIdString(int i){
+        return physproc.get(i).getIdString();
+    }
+    public int getPackageNumber(int i){
+        return physproc.get(i).getPhysicalPackageNumber();
+    }
+    public int processorEfficiency(int i){
+        return physproc.get(i).getEfficiency();
+    }
+    public void testData(){
+        System.out.printf("%-25s | %s\n","Processor ID: " , getIdString(0));
+        System.out.printf("%-25s | %s\n","Processor Package Number: " , getPackageNumber(0));
+        System.out.printf("%-25s | %s\n","Processor Core ID:" , physicalProcessorCount());
+        System.out.printf("%-25s | %s\n", "Processor Efficiency: " , processorEfficiency(0));
+        /*
+        System.out.println(physicalProcessorCount());
+        System.out.println(getIdString(0));
+        System.out.println(getPackageNumber(0));
+        System.out.println(processorEfficiency(0));
+        */
+        System.out.println(getCurrentFrequency(0));
+        System.out.println(getMaxFrequency(0));
+    }
+}
--- app/src/main/java/org/example/test.java
+++ app/src/main/java/org/example/test.java
@@ -7,6 +7,7 @@ public class test {
         LogicalCPU threadCpu = new LogicalCPU();
         PhysicalCPU physicalCpu = new PhysicalCPU();
         SysMemory memory = new SysMemory();
+
         specs.testData();
         physicalCpu.testData();
         threadCpu.testData();
