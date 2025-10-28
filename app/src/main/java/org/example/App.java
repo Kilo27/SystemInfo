f539c46bfffe926a93e711164c6159fcfd6e0143
ollie <oliverjcashen.student@gmail.com>
2025-10-28 10:30:05 +0000

    I'm just making sure I'm not an idiot

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,17 +3,44 @@
  */
 package org.example;
 
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+
 public class App {
     public static void main(String[] args){
+
+        // System Info
+        ProcessorSpecs specs = new ProcessorSpecs();
         OshiMethod oshi = new OshiMethod();
         LogicalCPU threadCpu = new LogicalCPU();
         PhysicalCPU physicalCpu = new PhysicalCPU();
-        ProcessorSpecs specs = new ProcessorSpecs();
         SysMemory memory = new SysMemory();
-        specs.testData();
+
+        // JFrame Parameters
+        Color backgroundColor = Color.decode("#211832"); // Background Colour
+        Color textColor = Color.decode("#FFFFFF"); // Text Colour
+        int windowWidth = 800;
+        int windowHeight = 600;
+
+        // Creating instance of JFrame
+        JFrame frame = new JFrame();
+        frame.setSize(windowWidth, windowHeight);
+        frame.setLayout(null); // using no layout managers
+        frame.setVisible(true); // making the frame visible
+        frame.getContentPane().setBackground(backgroundColor); // set background color
+
+        // Text Label
+        JLabel cpuNameLabel = new JLabel(String.format("Name: %s",specs.name()));
+        cpuNameLabel.setForeground(textColor);
+
+        cpuNameLabel.setBounds(100, 50, 300, 80);
+        frame.add(cpuNameLabel,BorderLayout.CENTER);
+
+        /*specs.testData();
         physicalCpu.testData();
         threadCpu.testData();
         memory.testData();
-        oshi.printSysUtil();
+        oshi.printSysUtil();*/
     }
 }
